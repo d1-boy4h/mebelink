3a91186baa261956496bbf1e384d964d05e02eab
d1_boy4h <d1.boy4h@gmail.com>
2026-09-22 04:26:17 +0300

    Прочие мелкие правки

--- src/constants/color_palette.py
+++ src/constants/color_palette.py
@@ -9,7 +9,7 @@ class ColorPalette(StrEnum):
     BRIGHT_TEXT = '#b1b7c2'
 
     RED    = '#ef4f79'
-    GREEN  = '#5bcb32'
+    GREEN  = "#4fb32b"
     YELLOW = "#f0aa2a"
     PURPLE = '#c44fef'
     GRAY   = '#8d8d8d'
